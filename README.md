111893c923e898906fb567b5883f4cd3b00cee31
SangwonJeon <31345532+allmie@users.noreply.github.com>
2020-06-14 17:26:21 +0900

    clock uint

---
+++ README.md
@@ -0,0 +1,6 @@
+# Features
+- [ ] Clock.
+- [ ] Username Persistance.
+- [ ] To Do List.
+- [ ] Random Background Image.
+- [ ] Weather with Geolocation.
---
+++ clock.js
@@ -0,0 +1,23 @@
+const clock = document.querySelector(".clock > h1");
+
+const loadTime = () => {
+    const time = new Date();
+
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    
+    clock.innerHTML = `${
+        hours < 10 ? `0{hours}` : hours
+    }:${
+        minutes < 10 ? `0{minutes}` : minutes
+    }:${
+        seconds < 10 ? `0${seconds}` : seconds}`
+}
+
+const init = () => {
+    loadTime();
+    setInterval(loadTime, 1000);
+};
+
+init();
